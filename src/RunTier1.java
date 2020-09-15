addad9dd63e4a5c643b9cda66f3c8f014922ab17
--replace-all <brodersen88@gmail.com>
2020-09-15 11:09:32 +0200

    Login

--- src/RunTier1.java
+++ src/RunTier1.java
@@ -1,8 +1,10 @@
 import tier1.Tier1ClientImpl;
 
+import java.rmi.RemoteException;
+
 public class RunTier1
 {
-  public static void main(String[] args)
+  public static void main(String[] args) throws RemoteException
   {
     Tier1ClientImpl tier1Client = new Tier1ClientImpl();
     tier1Client.startClient();
--- src/tier1/Tier1ClientImpl.java
+++ src/tier1/Tier1ClientImpl.java
@@ -7,24 +7,46 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Scanner;
 
 public class Tier1ClientImpl implements Tier1Client{
 
     private Tier2Server tier2Server;
 
-    public void startClient()
+    public boolean connectClient(int port)
     {
         try
         {
             UnicastRemoteObject.exportObject(this, 0);
-            Registry registry = LocateRegistry.getRegistry("localhost", 1098);
-            tier2Server = (Tier2Server) registry.lookup("BankTier2Server");
+            Registry registry = LocateRegistry.getRegistry("localhost", port);
+            tier2Server = (Tier2Server) registry.lookup("BankTier2Server" + port);
             System.out.println("Tier 1 client connected to tier 2 server.");
+            return true;
         }
         catch (RemoteException | NotBoundException e)
         {
             System.out.println("Could not connect to server");
             e.printStackTrace();
         }
+        return false;
+    }
+    public void startClient() throws RemoteException
+    {
+        int id, un;
+        String pass;
+        Scanner in = new Scanner(System.in);
+        do
+        {
+            System.out.println("Enter bank id:");
+            id = in.nextInt();
+        }while(!connectClient(id));
+        do
+        {
+            System.out.println("Enter userID:");
+            un = in.nextInt();
+            System.out.println("Enter password:");
+            pass = in.nextLine();
+        }
+        while(tier2Server.login(un,pass) == null);
     }
 }
